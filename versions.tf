9b5d9f3b4651548f00f57ef058daaecfd4450734
David Fischer <david@fisch3r.net>
2024-02-11 22:17:57 +0100

    Uprade providers

--- versions.tf
+++ versions.tf
@@ -4,17 +4,17 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = ">= 2.23.0"
+      version = ">= 3.0.2"
     }
 
     gitlab = {
       source  = "gitlabhq/gitlab"
-      version = ">= 3.20.0"
+      version = ">= 16.8.1"
     }
 
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.2.3"
+      version = ">= 2.4.1"
     }
   }
 }
