481a97c43b3d3152a3d7e4aa0c2e3a9404972b21
David Fischer <david@fisch3r.net>
2024-11-17 11:09:22 +0000

    Fix path

--- storage.tf
+++ storage.tf
@@ -1,13 +1,13 @@
 # Create jobs persistent directory
 
 resource "local_file" "jobs_storage_touch" {
-  filename             = "local.host_storage_directory/.touch"
+  filename             = "${local.host_storage_directory}/.touch"
   content              = "Used to create directory by TF."
   file_permission      = "0644"
   directory_permission = "0777"
 
   # https://github.com/hashicorp/terraform-provider-local/issues/366
   provisioner "local-exec" {
-    command = "mkdir -p ${local.host_storage_directory} && chmod 1777 ${local.host_storage_directory}"
+    command = "chmod 1777 ${local.host_storage_directory}"
   }
 }
